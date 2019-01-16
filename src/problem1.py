fb72958a859ff397128dfc884e5b08f7aaa0611a
colledkm <colledkm@rose-hulman.edu>
2019-01-15 20:30:25 -0500

    Done with problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -4,7 +4,7 @@ Exam 2, problem 1.
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
          and Katana Colledge.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -12,14 +12,14 @@ import time
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    # run_test_problem1a()
-    # run_test_problem1b()
-    # run_test_problem1c()
-    # run_test_problem1d()
+    run_test_problem1a()
+    run_test_problem1b()
+    run_test_problem1c()
+    run_test_problem1d()
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-strings for the  sum_of_digits  and  is_prime
+# Done: 2.  READ the doc-strings for the  sum_of_digits  and  is_prime
 # functions defined below.  They are the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -145,10 +145,10 @@ def problem1a(sequence):
       -- problem1a( (30, 1, 22, 8, 5) )  returns 35.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    return (sequence[0]+sequence[-1])
 
 def run_test_problem1b():
     """ Tests the   problem1b   function. """
@@ -293,10 +293,15 @@ def problem1b(strings):
            FOR HELP if this problem's specification is not clear to you.
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
-
+    answer = []
+    for k in range(len(strings)):
+        current = strings[k]
+        if is_prime(len(current)) is True:
+            answer.append(current)
+    return answer
 
 def run_test_problem1c():
     """ Tests the   problem1c   function. """
@@ -389,10 +394,14 @@ def problem1c(integers):
            their respective indices (0, 1, and 2, respectively).
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
-
+    for k in range(len(integers)):
+        current = integers[k]
+        if current < k:
+            return current
+    return 999
 
 def run_test_problem1d():
     """ Tests the   problem1d   function. """
@@ -523,10 +532,15 @@ def problem1d(t, sequence):
            FOR HELP if this problem's specification is not clear to you.
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
-
+    product = 1
+    for k in range(len(sequence)//2,len(sequence),1):
+        current = sequence[k]
+        if sum_of_digits(current) > t:
+            product = product*current
+    return product
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
